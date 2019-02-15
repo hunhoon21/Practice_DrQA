f477b540c5979265174a71ba38059ac5b8c0dab0
hunhoon21 <hunhoon21@gmail.com>
2019-02-16 01:57:59 +0900

    최상위 모델 클래스

---
+++ drqa/model.py
@@ -0,0 +1,154 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+import random
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+import logging
+
+from torch.autograd import Variable
+from .utils import AverageMeter
+from .rnn_reader import RnnDocReader
+
+# Modification:
+#   - change the logger name
+#   - save & load "state_dict"s of optimizer and loss meter
+#   - save all random seeds
+#   - change the dimension of inputs (for POS and NER features)
+#   - remove "reset parameters" and use a gradient hook for gradient masking
+# Origin: https://github.com/facebookresearch/ParlAI/tree/master/parlai/agents/drqa
+
+logger = logging.getLogger(__name__)
+
+class DocReaderModel(object):
+    """High level model that handles intializing the underlying network
+    architecture, saving, updating examples, and predicting examples.
+    """
+    # 가장 상위 개념의 클래스.
+    def __init__(self, opt, embedding=None, state_dict=None):
+        self.opt = opt
+        self.device = torch.cuda.current_device() if opt['cuda'] else torch.device('cpu')
+        self.updates = state_dict['update'] if state_dict else 0
+        self.train_loss = AverageMeter()
+        if state_dict:
+            self.train_loss.load(state_dict['loss'])
+
+        # 네트워크 통째로 가져오기!
+        self.network = RnnDocReader(opt, embedding=embedding)
+        if state_dict:
+            new_state = set(self.network.state_dict().keys())
+            for k in list(state_dict['network'].keys()):
+                if k not in new_state:
+                    del state_dict['network'][k]
+            self.network.load_state_dict(state_dict['network'])
+        self.network.to(self.device)
+
+        # Optimizer 만들기
+        self.opt_state_dict = state_dict['optimizer'] if state_dict else None
+        self.build_optimizer()
+
+    def build_optimizer(self):
+
+        parameters = [p for p in self.network.parameters() if p.requires_grad]
+        if self.opt['optimizer'] == 'sgd':
+            self.optimizer = optim.SGD(parameters, self.opt['learning_rate'],
+                                       momentum=self.opt['momentum'],
+                                       weight_decay=self.opt['weight_decay'])
+        elif self.opt['optimizer'] == 'adamax':
+            self.optimizer = optim.Adamax(parameters,
+                                          weight_decay=self.opt['weight_decay'])
+        else:
+            raise RuntimeError('Unsupported optimizer: %s' % self.opt['optimizer'])
+        if self.opt_state_dict:
+            self.optimizer.load_state_dict(self.opt_state_dict)
+
+    def update(self, ex):
+        # Train mode
+        self.network.train()
+
+        # device 장착, 난 cpu...
+        inputs = [e.to(self.device) for e in ex[:7]]
+        target_s = ex[7].to(self.device)
+        target_e = ex[8].to(self.divice)
+
+        # Run Forward, 근데 왜 forward가 실행이 될까?
+        score_s, score_e = self.network(*inputs)
+
+        # NLL
+        loss = F.nll_loss(score_s, target_s) + F.nll_loss(score_e, target_e)
+        self.train_loss.update(loss.item())
+
+        # Clear grads, Run Backward
+        self.optimizer.zero_grad()
+        loss.backward()
+
+        # Clip gradients, 무슨 역할을 할까?
+        torch.nn.utils.clip_grad_norm_(self.network.parameters(),
+                                       self.opt['gradient_clipping'])
+
+        # Update parameters
+        self.optimizer.step()
+        self.updates += 1
+
+    def predict(self, ex):
+        # Eval mode
+        self.network.eval()
+
+        # device 장착
+        if self.opt['cuda']:
+            #inputs = [Variable(e.cuda(async=True)) for e in ex[:7]]
+            inputs = [torch.tensor(e.cuda(async=True)) for e in ex[:7]]
+        else:
+            #inputs = [Variable(e) for e in ex[:7]]
+            inputs = [torch.tensor(e) for e in ex[:7]]
+
+        # Run Forward
+        with torch.no_grad():
+            score_s, score_e = self.network(*inputs)
+
+        # Transfer to CPU / normal tensors for numpy ops
+        score_s = score_s.data.cpu()
+        score_e = score_e.data.cpu()
+
+        # Get argmax text spans
+        text = ex[-2]
+        spans = ex[-1]
+        predictions = []
+        max_len = self.opt['max_len'] or score_s.size(1)
+        for i in range(score_s.size(0)):
+            scores = torch.ger(score_s[i], score_e[i])
+            scores.triu_().tril_(max_len - 1)
+            scores = scores.numpy()
+            s_idx, e_dix = np.unravel_index(np.argmax(scores), scores.shape)
+            s_offset, e_offset = spans[i][s_idx][0], spans[i][e_dix][1]
+            predictions.append(text[i][s_offset:e_offset])
+
+        return predictions
+
+    def save(self, filename, epoch, scores):
+        em, f1, best_eval = scores
+        params = {
+            'state_dict': {
+                'network': self.network.state_dict(),
+                'optimizer': self.optimizer.state_dict(),
+                'updates': self.updates,
+                'loss': self.train_loss.state_dict()
+            },
+            'config': self.opt,
+            'epoch': epoch,
+            'em': em,
+            'f1': f1,
+            'best_eval': best_eval,
+            'random_state': random.getstate(),
+            'torch_state': torch.random.get_rng_state(),
+            'torch_cuda_state': torch.cuda.get_rng_state()
+        }
+        try:
+            torch.save(params, filename)
+            logger.info('model saved to {}' .format(filename))
+        except BaseException:
+            logger.warning('[WARN: Saving failed.... continuing anyway. ]')
